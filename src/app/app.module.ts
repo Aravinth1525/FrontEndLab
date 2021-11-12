a2c2462fc402153bc7e239542ca813d50b3a30ee
Aravinth <aravinth@Aravinths-MacBook-Air.local>
2021-11-12 19:27:33 +0530

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { SpecialComponent } from './special/special.component';
+import { AboutComponent } from './about/about.component';
+import { BestComponent } from './best/best.component';
+import { HistoryComponent } from './history/history.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    SpecialComponent,
+         AboutComponent,
+         BestComponent,
+         HistoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'special', component: SpecialComponent},
+      {path: 'about', component: AboutComponent},
+      {path: 'best', component: BestComponent},
+      {path: 'history', component: HistoryComponent},
+                                 
+        
+
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
